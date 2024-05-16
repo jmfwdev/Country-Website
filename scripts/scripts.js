58b61beaecd3ab5eb30cf88db2d3734ae1131544
JM Hore <kira@JMs-MacBook-Air.local>
2024-05-16 00:27:33 -0700

    Styled the header by putting a hamburger menu

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,2 +1,8 @@
 "use strict";
 
+const navButton = document.getElementById("navButton");
+const navLinks = document.getElementById("navList");
+
+navButton.addEventListener("click", function () {
+    navLinks.classList.toggle("toggled");
+})
